719b0e5fa1814742fd1fe73c568425cecfde93d4
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-05 05:55:48 +0330

    refactor(pint): clean with pint

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -73,51 +73,51 @@ class AuthController extends Controller
         if ($errors['errors']) {
             Flight::json($this->fail($errors, 403), 422);
         } else {
-            $email = $request["email"];
-            $password = $request["password"];
+            $email = $request['email'];
+            $password = $request['password'];
             $db = Flight::db();
             $stmt = $db->prepare('SELECT * FROM users WHERE email = :email');
-            $stmt->execute([":email" => $email]);
+            $stmt->execute([':email' => $email]);
             $stmt = $stmt->fetchAll();
             $user = end($stmt);
-            if (!$user) {
+            if (! $user) {
                 Flight::json($this->fail([
-                    "errors" => [
-                        "user" => "user not fund."
-                    ]
+                    'errors' => [
+                        'user' => 'user not fund.',
+                    ],
                 ], 422), 422);
             } else {
-                if (password_verify($password, $user["password"])) {
+                if (password_verify($password, $user['password'])) {
                     date_default_timezone_set('UTC');
                     $today = date('Y-m-d');
-                    $tokenCount = $db->prepare("SELECT COUNT(*) as token_count FROM tokens WHERE DATE(created_at) = :today");
+                    $tokenCount = $db->prepare('SELECT COUNT(*) as token_count FROM tokens WHERE DATE(created_at) = :today');
                     $tokenCount->execute([
-                        ":today" => $today
+                        ':today' => $today,
                     ]);
                     $tokenCount = $tokenCount->fetchAll();
-                    if (intval(end($tokenCount)["token_count"]) < 100) {
-                        $token = $this->generateJWT($user, "ijliuyiu");
+                    if (intval(end($tokenCount)['token_count']) < 100) {
+                        $token = $this->generateJWT($user, 'ijliuyiu');
                         $stmt2 = $db->prepare('INSERT INTO `tokens` (`token`, `user_id`) VALUES (:token, :user_id)');
                         $stmt2->execute([
-                            ":token" => $token,
-                            ":user_id" => $user["id"]
+                            ':token' => $token,
+                            ':user_id' => $user['id'],
                         ]);
                         Flight::json($this->success([
-                            "today_count" => end($tokenCount)["token_count"],
-                            "token" => $token
+                            'today_count' => end($tokenCount)['token_count'],
+                            'token' => $token,
                         ]));
                     } else {
                         Flight::json($this->fail([
-                            "errors" => [
-                                "token" => "You have generated the maximum number of tokens. You cannot generate another token for 24 hours"
-                            ]
+                            'errors' => [
+                                'token' => 'You have generated the maximum number of tokens. You cannot generate another token for 24 hours',
+                            ],
                         ], 403), 403);
                     }
                 } else {
                     Flight::json($this->fail([
-                        "errors" => [
-                            "password" => "password incorrect."
-                        ]
+                        'errors' => [
+                            'password' => 'password incorrect.',
+                        ],
                     ], 422), 422);
                 }
             }
--- app/Controllers/Controller.php
+++ app/Controllers/Controller.php
@@ -4,26 +4,25 @@ namespace App\Controllers;
 
 use App\Trait\BaseApiTrait;
 use Firebase\JWT\JWT;
-use Firebase\JWT\Key;
 
 class Controller
 {
     use BaseApiTrait;
 
-    function generateJWT($user, $key): string
+    public function generateJWT($user, $key): string
     {
         $secret_key = $key;
 
         $payload = [
-            'iss' => env("APP_URL"),
-            'aud' => env("APP_URL") . "/" . "auth",
+            'iss' => env('APP_URL'),
+            'aud' => env('APP_URL').'/'.'auth',
             'iat' => time(),
             'exp' => time() + ((3600 * 24) * 30),
             'data' => [
-                'id' => $user["id"],
-                'name' => $user["name"],
-                'email' => $user["email"]
-            ]
+                'id' => $user['id'],
+                'name' => $user['name'],
+                'email' => $user['email'],
+            ],
         ];
 
         $jwt = JWT::encode($payload, $secret_key, 'HS256');
