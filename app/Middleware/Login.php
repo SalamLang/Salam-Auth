b69fd8d3a5bf94deebac44fc0e98fb3ab2bcbe99
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-07 14:29:48 +0330

    feat(middleware): add login middleware for check user

---
+++ app/Middleware/Login.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Middleware;
+
+use App\Controllers\Controller;
+use Flight;
+
+class Login extends Controller
+{
+    public function before(): void
+    {
+        if (isset(getallheaders()["Authorization"])) {
+            $header_token = getallheaders()["Authorization"];
+            if (str_starts_with($header_token, "Bearer")) {
+                $header_token = substr($header_token, 7);
+            }
+            $db = Flight::db();
+            $tokenCount = $db->prepare('SELECT COUNT(*) as token_count FROM tokens WHERE `token` = :token');
+            $tokenCount->execute([
+                ':token' => $header_token,
+            ]);
+            $result = $tokenCount->fetchAll();
+            $result = end($result)["token_count"];
+            if (intval($result) === 0) {
+                Flight::jsonHalt($this->success([
+                    "message" => ["Unauthorized"]
+                ], "401"), 401);
+            }
+        } else {
+            Flight::jsonHalt($this->success([
+                "message" => ["Unauthorized"]
+            ], "401"), 401);
+        }
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Controllers\AuthController;
+use App\Middleware\Login;
 
 Flight::group('/api/v1', function () {
     Flight::route('POST /auth', [new AuthController, 'auth']);
@@ -8,14 +9,6 @@ Flight::group('/api/v1', function () {
     Flight::route('POST /login', [new AuthController, 'login']);
     Flight::route('POST /register', [new AuthController, 'register']);
     Flight::route('POST /test', function () {
-        $db = Flight::db();
-        $request = Flight::request()->data->getData();
-        $token = $request["token"];
-        $tokenCount = $db->prepare('SELECT COUNT(*) as token_count FROM tokens WHERE `token` = :token');
-        $tokenCount->execute([
-            ':token' => $token,
-        ]);
-        $result = $tokenCount->fetchAll();
-        print_r($result);
-    });
+        echo "hi";
+    })->addMiddleware([new Login()]);
 });
