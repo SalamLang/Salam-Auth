0eb43c3043416073319479ee8b1bd6a64bd60fb8
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-07 13:36:49 +0330

    feat(database): change database token field ttype

--- routes/api.php
+++ routes/api.php
@@ -8,6 +8,14 @@ Flight::group('/api/v1', function () {
     Flight::route('POST /login', [new AuthController, 'login']);
     Flight::route('POST /register', [new AuthController, 'register']);
     Flight::route('POST /test', function () {
-
+        $db = Flight::db();
+        $request = Flight::request()->data->getData();
+        $token = $request["token"];
+        $tokenCount = $db->prepare('SELECT COUNT(*) as token_count FROM tokens WHERE `token` = :token');
+        $tokenCount->execute([
+            ':token' => $token,
+        ]);
+        $result = $tokenCount->fetchAll();
+        print_r($result);
     });
 });
