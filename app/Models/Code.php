702d7529979df9f3c3610a16fbdac6a866d75f9f
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-07 20:46:15 +0330

    feat(code): add code model

--- app/Models/Code.php
+++ app/Models/Code.php
@@ -6,19 +6,19 @@ class Code extends Main
 {
     public static function all()
     {
-        return Main::getAll('tokens');
+        return Main::getAll('codes');
     }
 
     public static function find($id)
     {
-        $array = Main::getFind('tokens', $id);
+        $array = Main::getFind('codes', $id);
 
         return end($array);
     }
 
     public static function where($parameter1, $parameter2)
     {
-        $array = Main::getWhere('tokens', $parameter1, $parameter2);
+        $array = Main::getWhere('codes', $parameter1, $parameter2);
 
         return end($array);
     }
