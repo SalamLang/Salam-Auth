cd0624a1b622bb063ddfabf136e697725ca84e37
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-08 17:29:22 +0330

    feat(users): add users controller

---
+++ app/Controllers/admin/users/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controllers\admin\users;
+
+use App\Controllers\Controller;
+
+class UserController extends Controller
+{
+    public function index(): void
+    {
+        echo "dfk";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Class\Route;
-use App\Controllers\admin\HomeController;
+use App\Controllers\admin\users\UserController;
 use App\Controllers\AuthController;
 use App\Controllers\IndexController;
 use App\Middleware\mvc\Admin;
@@ -14,6 +14,7 @@ Route::lists([
     'index' => $APP_URL,
     'admin.home' => $APP_URL.'/'.'admin',
     'new_password' => $APP_URL.'/'.'new_password',
+    'users.index' => $APP_URL.'/'.'admin/users',
 ]);
 
 Flight::route('GET /auth', [new AuthController, 'index']);
@@ -24,6 +25,12 @@ Flight::route('GET /', [new IndexController, 'index']);
 
 Flight::group('/', function () {
     Flight::group('admin', function () {
-        Flight::route('GET /', [new HomeController, 'index']);
+
+        Flight::route('GET /', [new UserController, 'index']);
+
+        Flight::group("/users", function(){
+            Flight::route('GET /', [new UserController, 'index']);
+        });
+
     }, [new Admin]);
 }, [new Login]);
