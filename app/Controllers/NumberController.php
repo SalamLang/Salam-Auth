2f7b88895654e6807ffda5c4b24b4c2d6839ab67
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-09 14:20:08 +0330

    feat(function): add function

--- app/Controllers/NumberController.php
+++ app/Controllers/NumberController.php
@@ -2,7 +2,27 @@
 
 namespace App\Controllers;
 
+use App\Models\Number;
+use Flight;
+
 class NumberController extends Controller
 {
-    public function new_password(): void {}
+    public function new_password($num): void {
+        $number = Number::where('number', $num);
+        if($number){
+            $request = Flight::request()->data->getData();
+            $db = Flight::db();
+            $stmt = $db->prepare("INSERT INTO `numbers`(`number`) VALUES (:number)");
+            $stmt->execute([
+                "number" => $request["number"]
+            ]);
+            Flight::json([
+                "message" => ["The phone number has been successfully registered."]
+            ]);
+        }else {
+            Flight::json([
+                "message" => ["The number is already registered."]
+            ]);
+        }
+    }
 }
