ca5657255bb234b42f1b7d0118b5e5f31f45e238
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-09 14:36:06 +0330

    feat(header): add code index route to header

--- resources/views/admin/layouts/partials/header.blade.php
+++ resources/views/admin/layouts/partials/header.blade.php
@@ -78,6 +78,14 @@
                             <span class="nav-link-title">کاربران</span>
                         </a>
                     </li>
+                    <li class="nav-item @if(substr(in_route(), 0, strpos(in_route(), '?') !== false ? strpos(in_route(), '?') : strlen(in_route())) === route("codes.index")) active @endif">
+                        <a class="nav-link" href="{{ route("codes.index") }}">
+                            <span class="nav-link-icon d-md-none d-lg-inline-block">
+                              <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-code"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M7 8l-4 4l4 4" /><path d="M17 8l4 4l-4 4" /><path d="M14 4l-4 16" /></svg>
+                            </span>
+                            <span class="nav-link-title">کد ها</span>
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
