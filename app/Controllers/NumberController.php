8c074f204e46fd9c69df3f7a965a3e5e0c125615
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-09 16:50:56 +0330

    fix(type): fix type bug

--- app/Controllers/NumberController.php
+++ app/Controllers/NumberController.php
@@ -17,7 +17,7 @@ class NumberController extends Controller
             $db = Flight::db();
             $stmt = $db->prepare('INSERT INTO `numbers`(`number`) VALUES (:number)');
             $stmt->execute([
-                'number' => intval($request['number']),
+                ':number' => intval($request['number']),
             ]);
             Flight::json([
                 'message' => ['شماره تلفن با موفقیت ثبت شد.'],
