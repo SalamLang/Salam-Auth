0d4e5ddfe4bfa6a4ec54231202a82c1209887710
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-09 17:05:07 +0330

    feat(error): handle valdation error

--- app/Controllers/NumberController.php
+++ app/Controllers/NumberController.php
@@ -4,28 +4,38 @@ namespace App\Controllers;
 
 use App\Models\Number;
 use Flight;
+use GeekGroveOfficial\PhpSmartValidator\Validator\Validator;
 
 class NumberController extends Controller
 {
     public function new_password(): void
     {
         $request = Flight::request()->data->getData();
-        $number = Number::where('number', $request["number"]);
-
-        if (!$number) {
-            $request = Flight::request()->data->getData();
-            $db = Flight::db();
-            $stmt = $db->prepare('INSERT INTO `numbers`(`number`) VALUES (:number)');
-            $stmt->execute([
-                ':number' => $request['number'],
-            ]);
-            Flight::json([
-                'message' => ['شماره تلفن با موفقیت ثبت شد.'],
-            ]);
-        } else {
-            Flight::json([
-                'message' => ['شماره از قبل وجود دارد.'],
-            ]);
+        $rules = [
+            'number' => ['required', 'int', "max:11", "min:11"]
+        ];
+        $validator = new Validator($request, $rules);
+        $validator->validate();
+        $errors = $validator->errors();
+        if ($errors){
+            Flight::json($this->fail(["errors" => $errors]));
+        }else {
+            $number = Number::where('number', $request["number"]);
+            if (!$number) {
+                $request = Flight::request()->data->getData();
+                $db = Flight::db();
+                $stmt = $db->prepare('INSERT INTO `numbers`(`number`) VALUES (:number)');
+                $stmt->execute([
+                    ':number' => $request['number'],
+                ]);
+                Flight::json($this->success([
+                    'message' => ['شماره تلفن با موفقیت ثبت شد.'],
+                ]));
+            } else {
+                Flight::json($this->fail([
+                    'message' => ['شماره از قبل وجود دارد.'],
+                ]));
+            }
         }
     }
 }
