5ffe50c5caec3ca9f31ada8ef1f5fe26860cca52
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-09 14:10:23 +0330

    refactor(pint): clean code with pint

--- app/Controllers/NumberController.php
+++ app/Controllers/NumberController.php
@@ -2,19 +2,7 @@
 
 namespace App\Controllers;
 
-use App\Class\Mail;
-use App\Models\ForgotToken;
-use App\Models\User;
-use DateTime;
-use Exception;
-use Flight;
-use GeekGroveOfficial\PhpSmartValidator\Validator\Validator;
-use Random\RandomException;
-
 class NumberController extends Controller
 {
-    public function new_password(): void
-    {
-
-    }
+    public function new_password(): void {}
 }
