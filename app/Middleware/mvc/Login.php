ea8e959abe3c6fd302ce4c5ec4cf39b36b119008
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-12 00:38:35 +0330

    bug(middleware)

--- app/Middleware/mvc/Login.php
+++ app/Middleware/mvc/Login.php
@@ -10,20 +10,9 @@ class Login extends Controller
     public function before(): void
     {
         if (isset($_COOKIE['token'])) {
-            dd($_COOKIE);
-            $header_token = $_COOKIE['token'];
-            $db = Flight::db();
-            $tokenCount = $db->prepare('SELECT COUNT(*) as token_count FROM tokens WHERE `token` = :token');
-            $tokenCount->execute([
-                ':token' => $header_token,
-            ]);
-            $result = $tokenCount->fetchAll();
-            $result = end($result)['token_count'];
-            if (intval($result) === 0) {
-                Flight::redirect("https://editor.salamlang.ir");
-            }
+            dd("ijj");
         } else {
-            Flight::redirect("https://editor.salamlang.ir");
+            dd("ijj");
         }
     }
 }
