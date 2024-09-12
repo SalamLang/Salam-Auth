e1ea6a19d2e402d027cecfe391b87217db9db205
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-12 23:27:39 +0330

    feat(lang): chnage lang

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -15,3 +16,6 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
+Route::get("/auth", [AuthController::class, "auth"])->name("auth");
