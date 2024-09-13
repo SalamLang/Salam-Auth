a2886e187cb4b1ef92e77a0c37c580e895dbc1c2
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-13 16:33:10 +0330

    feat(error): handle error

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,3 +5,13 @@ import Alpine from 'alpinejs';
 window.Alpine = Alpine;
 
 Alpine.start();
+
+const elm_inputs = document?.querySelectorAll('input')
+
+elm_inputs?.forEach((item) => {
+    let next = item?.nextElementSibling?.tagName?.toLowerCase()
+
+    if (next === "ul"){
+        item.classList.add("invalid")
+    }
+})
