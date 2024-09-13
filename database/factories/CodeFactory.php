af7f033727158c18efa6b66493fb4993eb5eead0
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-14 03:04:58 +0330

    feat(factory): add code factory

---
+++ database/factories/CodeFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Code>
+ */
+class CodeFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'uuid' => fake()->uuid(),
+            'title' => fake()->sentence(),
+            'code' => fake()->paragraph(),
+            'user_id' => User::factory(),
+        ];
+    }
+}
