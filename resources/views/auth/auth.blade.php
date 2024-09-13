9e0d986c8e5a213d22203b8aab34a51ff17d5d78
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-13 15:26:44 +0330

    feat(auth): add auth style

--- resources/views/auth/auth.blade.php
+++ resources/views/auth/auth.blade.php
@@ -1,35 +1,23 @@
 <x-guest-layout>
-    <x-authentication-card>
-        <x-authentication-card-logo/>
+    <x-auth-session-status class="mb-4" :status="session('status')"/>
 
-        <x-validation-errors class="my-4 w-full"/>
+    <x-authentication-card-logo/>
 
-        <h1 class="text-black mt-1.5 font-bold text-[26px]">به جمع ما بپیوند!</h1>
+    <h1 class="text-black mt-1.5 font-bold text-[26px]">به جمع ما بپیوند!</h1>
 
-        @session('status')
-        <div class="mb-4 font-medium text-sm text-green-600">
-            {{ $value }}
+    <form method="POST" action="{{ route('auth') }}" class="w-full">
+        @csrf
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')"/>
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                          autofocus autocomplete="username"/>
+            <x-input-error :messages="$errors->get('email')" class="mt-2"/>
         </div>
-        @endsession
-        <form method="POST" action="{{ route('auth') }}" class="w-full">
-            @csrf
-            <div>
-                <x-label for="email" value="{{ __('Email') }}"/>
-                <x-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
-                         autofocus autocomplete="username"/>
-            </div>
-            @if (Route::has('password.request'))
-                <div class="input-box w-full mt-4 mb-2">
-                    <a href="{{ route('password.request') }}"
-                       class="forgot-password text-[#276EF6] font-bold hover:underline">{{ __('Forgot your password?') }}</a>
-                </div>
-            @endif
 
-            <div class="input-box w-full">
-                <x-button-auth>
-                    {{ __('مرحله بعد') }}
-                </x-button-auth>
-            </div>
-        </form>
-    </x-authentication-card>
+        <x-button-auth>
+            {{ __('Log in') }}
+        </x-button-auth>
+    </form>
+
 </x-guest-layout>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,24 +1,61 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet"/>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans text-gray-900 antialiased auth_body">
-        <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white dark:bg-gray-900">
-            <div class="auth transition-all duration-300 max-h-[93%] max-w-[450px] overflow-auto fixed top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 px-[40px] w-full cus-bg rounded-[20px] flex flex-col justify-start items-center py-5">
-                {{ $slot }}
-            </div>
-        </div>
-    </body>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+    <style>
+        @font-face {
+            font-family: "estedad";
+            font-weight: 100;
+            src: url({{ asset("assets/fonts/Estedad-Thin.ttf") }});
+        }
+
+        @font-face {
+            font-family: "estedad";
+            font-weight: 300;
+            src: url({{ asset("assets/fonts/Estedad-Light.ttf") }});
+        }
+
+        @font-face {
+            font-family: "estedad";
+            font-weight: 400;
+            src: url({{ asset("assets/fonts/Estedad-Medium.ttf") }});
+        }
+
+        @font-face {
+            font-family: "estedad";
+            font-weight: 700;
+            src: url({{ asset("assets/fonts/Estedad-Bold.ttf") }});
+        }
+
+        @font-face {
+            font-family: "estedad";
+            font-weight: 900;
+            src: url({{ asset("assets/fonts/Estedad-Black.ttf") }});
+        }
+
+        * {
+            font-family: "estedad", sans-serif !important;
+        }
+    </style>
+</head>
+<body class="font-sans text-gray-900 antialiased auth_body">
+<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-white dark:bg-gray-900">
+    <div
+        class="auth transition-all duration-300 max-h-[93%] max-w-[450px] overflow-auto fixed top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 px-[40px] w-full cus-bg rounded-[20px] flex flex-col justify-start items-center py-5">
+        {{ $slot }}
+    </div>
+</div>
+</body>
 </html>
