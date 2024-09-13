99093b6f690c87bcb2cd30f792b05c99133ca237
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-13 14:11:42 +0330

    feat(remove): remove laravel project

--- resources/views/components/authentication-card.blade.php
+++
@@ -1,5 +0,0 @@
-<div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0">
-    <div class="auth transition-all duration-300 max-h-[93%] max-w-[450px] overflow-auto fixed top-1/2 right-1/2 -translate-y-1/2 translate-x-1/2 px-[40px] w-full cus-bg rounded-[20px] flex flex-col justify-start items-center py-5">
-        {{ $slot }}
-    </div>
-</div>
--- resources/views/components/input.blade.php
+++
@@ -1,3 +0,0 @@
-@props(['disabled' => false])
-
-<input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge(['class' => 'border-2 border-transparent transition-all duration-300 w-full mt-1.5 h-[50px] rounded-[15px] !outline-0 p-3 placeholder-gray-300']) !!}>
