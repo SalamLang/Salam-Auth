50ab64675dda19c92078918e54b906f0cebf0041
mohamadreza <mohamadreza1388.org@gmail.com>
2024-09-16 12:01:33 +0330

    feat(code history): add code history chart

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Code;
 use App\Models\User;
 use Carbon\Carbon;
 
@@ -10,9 +11,9 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        function getUsers($day)
+        function GetTables($day, $model)
         {
-            $data = User::where('created_at', '>=', Carbon::now()->subDays($day))
+            $data = $model::where('created_at', '>=', Carbon::now()->subDays($day))
                 ->selectRaw('DATE(created_at) as date, COUNT(*) as total')
                 ->groupBy('date')
                 ->orderBy('date', 'ASC')
@@ -27,10 +28,12 @@ class DashboardController extends Controller
             ];
         }
 
-        $users_history = getUsers(10);
+        $users_history = GetTables(10, User::class);
+        $codes_history = GetTables(10, Code::class);
 
         return view('admin.dashboard', [
             'users_history' => $users_history,
+            'codes_history' => $codes_history,
         ]);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -11,6 +11,15 @@
                 </div>
                 <div id="small_chart_1" class="chart-sm"></div>
             </div>
+            <div class="card basis-1/4 border-0 shadow !rounded-[15px] !overflow-hidden">
+                <div class="card-body">
+                    <div class="d-flex align-items-center justify-content-between">
+                        <div class="subheader fs-4">تعداد کد ها</div>
+                        <h3 class="m-0">{{ array_sum($codes_history["totals"]) }}</h3>
+                    </div>
+                </div>
+                <div id="small_chart_2" class="chart-sm"></div>
+            </div>
         </div>
 
     </div>
@@ -73,6 +82,64 @@
                         },
                     },
                     labels: @json($users_history["dates"]),
+                    colors: ["#FF5C00"],
+                    legend: {
+                        show: false,
+                    },
+                })).render();
+
+                window.ApexCharts && (new ApexCharts(document.getElementById('small_chart_2'), {
+                    chart: {
+                        type: "area",
+                        fontFamily: 'inherit',
+                        height: 70.0,
+                        sparkline: {
+                            enabled: true
+                        },
+                        animations: {
+                            enabled: true
+                        },
+                    },
+                    dataLabels: {
+                        enabled: false,
+                    },
+                    fill: {
+                        opacity: .16,
+                        type: 'solid'
+                    },
+                    stroke: {
+                        width: 2,
+                        lineCap: "round",
+                        curve: "smooth",
+                    },
+                    series: [{
+                        name: "تعداد",
+                        data: @json($codes_history["totals"])
+                    }],
+                    tooltip: {
+                        theme: 'dark'
+                    },
+                    grid: {
+                        strokeDashArray: 4,
+                    },
+                    xaxis: {
+                        labels: {
+                            padding: 0,
+                        },
+                        tooltip: {
+                            enabled: false
+                        },
+                        axisBorder: {
+                            show: false,
+                        },
+                        type: 'datetime',
+                    },
+                    yaxis: {
+                        labels: {
+                            padding: 4
+                        },
+                    },
+                    labels: @json($codes_history["dates"]),
                     colors: ["#FF5C00"],
                     legend: {
                         show: false,
